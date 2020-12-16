97449d64c57a5fa98ab0324e3467400131d8b895
Alec West <alecwest38@gmail.com>
2020-12-16 16:12:12 -0600

    day 1 part 2

--- src/main/scala/advent/puzzles/SumTo.scala
+++ src/main/scala/advent/puzzles/SumTo.scala
@@ -1,33 +1,15 @@
 package com.alecnwest
 package advent.puzzles
 
-import monix.eval.Coeval
-
 /**
- * Day 1, challenge 1
+ * Day 1
  */
 object SumTo {
-  val targetSum = 2020
-
-  def calculate(input: IndexedSeq[Int]): Int = {
-    val sorted = input.sorted
-    var i = 0
-    var j = sorted.length - 1
-    while (j > 0) {
-      i = 0
-      val highVal = sorted(j)
-      val lowVal = sorted(i)
-      while (i < j) {
-        val sum: Coeval[Int] = Coeval.delay(highVal + lowVal)
-        if (highVal < lowVal || sum.value > targetSum) {
-          i = j // Force inner loop to exit early
-        } else if (sum.value == targetSum) {
-          return lowVal * highVal
-        }
-        i += 1
-      }
-      j -= 1
-    }
-    -1
+  def calculate(input: IndexedSeq[Int], numbersToSum: Int = 2, targetSum: Int = 2020): Int = {
+    input
+      .combinations(numbersToSum)
+      .find(combo => combo.sum == targetSum)
+      .getOrElse(IndexedSeq(-1))
+      .product
   }
 }
