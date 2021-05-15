7d62bdfba8399d876f206d261ce652779f9508ba
Alec West <alecwest38@gmail.com>
2021-05-15 14:55:16 -0500

    day 5 part 2

--- src/main/scala/advent/puzzles/binaryboarding/BinaryBoarding.scala
+++ src/main/scala/advent/puzzles/binaryboarding/BinaryBoarding.scala
@@ -3,7 +3,23 @@ package advent.puzzles.binaryboarding
 
 object BinaryBoarding {
   def highestSeatID(input: IndexedSeq[String]): Int =
-    input
-      .map(line => BoardingPass.fromString(line).map(_.seatID).getOrElse(0))
-      .max
+    getSeatIDs(input).max
+
+  def findMissingSeat(input: IndexedSeq[String]): Int = {
+    val seatIDs = getSeatIDs(input).sorted
+    seatIDs.zipWithIndex
+      .find {
+        case (seatID, index) =>
+          if (index > 0 && index < seatIDs.length - 1) {
+            seatID - seatIDs(index - 1) != 1
+          } else {
+            false
+          }
+      }
+      .getOrElse((0, 0))
+      ._1 - 1
+  }
+
+  private def getSeatIDs(input: IndexedSeq[String]): IndexedSeq[Int] =
+    input.map(line => BoardingPass.fromString(line).map(_.seatID).getOrElse(0))
 }
