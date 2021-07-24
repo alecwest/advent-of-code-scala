df885064e5fff00995e719da3e6c6fa77afd3162
Alec West <alecwest38@gmail.com>
2021-07-24 15:15:14 -0500

    day 9 part 2

--- src/main/scala/advent/puzzles/encodingerror/EncodingError.scala
+++ src/main/scala/advent/puzzles/encodingerror/EncodingError.scala
@@ -2,23 +2,37 @@ package com.alecnwest
 package advent.puzzles.encodingerror
 
 object EncodingError {
-  def firstInvalidNumber(input: IndexedSeq[BigInt], preambleLength: Int): Number = {
+  def firstInvalidNumber(input: IndexedSeq[Long], preambleLength: Int): Long = {
     (input.zipWithIndex.slice(preambleLength, input.size) find {
-      case (nextNumber: BigInt, index: Int) => {
+      case (nextNumber: Long, index: Int) =>
         !(input.zipWithIndex.slice(index - preambleLength, index) exists {
-          case (firstNumber: BigInt, j: Int) => {
+          case (firstNumber: Long, j: Int) =>
             input.zipWithIndex.slice(index - preambleLength, index) exists {
-              case (secondNumber: BigInt, k: Int) => {
+              case (secondNumber: Long, k: Int) =>
                 if (j == k) {
                   false
                 } else {
                   firstNumber + secondNumber == nextNumber
                 }
-              }
             }
-          }
         })
-      }
     }).map(_._1).get
+  }
+
+  def encryptionWeakness(input: IndexedSeq[Long], preambleLength: Int): Long = {
+    val firstInvalid = firstInvalidNumber(input, preambleLength)
+    for ((_, i: Int) <- input.zipWithIndex) {
+      var sum: Long = 0
+      val weakNumbers = input.slice(i, input.size).takeWhile { next =>
+        if (sum < firstInvalid) {
+          sum += next
+          true
+        } else
+          false
+      }
+      if (sum == firstInvalid)
+        return weakNumbers.min + weakNumbers.max
+    }
+    0
   }
 }
