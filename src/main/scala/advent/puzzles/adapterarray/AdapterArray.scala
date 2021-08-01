ce5606c40dc2a6f958fd89146d9cdb7d2c8081a0
Alec West <alecwest38@gmail.com>
2021-08-01 12:53:27 -0500

    day 10 part 2

---
+++ src/main/scala/advent/puzzles/adapterarray/AdapterArray.scala
@@ -0,0 +1,53 @@
+package com.alecnwest
+package com.alecnwest.advent.puzzles.adapterarray
+import scala.collection.mutable
+
+object AdapterArray {
+  def numberPossibleArrangements(input: IndexedSeq[Int]): Long = {
+    val sorted = 0 +: input.sorted :+ input.max + 3
+    val sequenceCache = mutable.Map(0 -> 0, 1 -> 1, 2 -> 1, 3 -> 2, 4 -> 4, 5 -> 7, 6 -> 13)
+    var contiguousSingleAdapterCounter = Array(1)
+    // count contiguous single difference adapters
+    // add this to an array of values that get multiplied at the end
+    for((adapterRating, index) <- sorted.zipWithIndex) {
+      if (index != 0) {
+        adapterRating - sorted(index - 1) match {
+          case 1 =>
+            contiguousSingleAdapterCounter = contiguousSingleAdapterCounter.slice(
+              0, contiguousSingleAdapterCounter.length - 1).concat(
+                Array(contiguousSingleAdapterCounter.last + 1))
+          case 2 => throw new Exception("uhhh?")
+          case 3 =>
+            contiguousSingleAdapterCounter = contiguousSingleAdapterCounter.concat(Array(1))
+        }
+      }
+    }
+
+    val uhh = contiguousSingleAdapterCounter.filter(_ > 2).map(count => {
+      if (!sequenceCache.contains(count)) {
+        val result = sequenceCache(count - 3) + sequenceCache(count - 2) + sequenceCache(count - 1)
+        sequenceCache(count) = result
+      }
+      sequenceCache(count).toLong
+    })
+    uhh.product
+  }
+
+  def product1And3Differences(input: IndexedSeq[Int]): Int = {
+    val diffs = countDifferences(input)
+    diffs._1 * diffs._3
+  }
+
+  private def countDifferences(input: IndexedSeq[Int]): (Int, Int, Int) = {
+    var (a, b, c) = (0, 0, 0)
+    val sorted = input.sorted
+    for ((adapterRating, index) <- sorted.zipWithIndex) {
+      adapterRating - (if (index == 0) 0 else sorted(index - 1)) match {
+        case 1 => a += 1
+        case 2 => b += 1
+        case 3 => c += 1
+      }
+    }
+    (a, b, c+1)
+  }
+}
---
+++ src/test/scala/advent/puzzles/adapterarray/AdapterArrayTest.scala
@@ -0,0 +1,62 @@
+package com.alecnwest
+package com.alecnwest.advent.puzzles.adapterarray
+
+import com.alecnwest.advent.utils.InputParser
+import org.scalatest.{Matchers, WordSpecLike}
+
+class AdapterArrayTest extends WordSpecLike with Matchers {
+
+  "AdapterArrayTest" when {
+    "product1And3Differences" should {
+      "return the product of example input" in {
+        AdapterArray.product1And3Differences(
+          InputParser.parse[Int](
+            "28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3"
+          )
+        ) should be(220)
+      }
+
+      "return the product of puzzle input" in {
+        AdapterArray.product1And3Differences(
+          InputParser.parse[Int](
+            "80\n87\n10\n122\n57\n142\n134\n59\n113\n139\n101\n41\n138\n112\n46\n96\n43\n125\n36\n54\n133\n17\n42\n98\n7\n114\n78\n67\n77\n28\n149\n58\n20\n105\n31\n19\n18\n27\n40\n71\n117\n66\n21\n72\n146\n90\n97\n94\n123\n1\n119\n30\n84\n61\n91\n118\n2\n29\n104\n73\n13\n76\n24\n148\n68\n111\n131\n83\n49\n8\n132\n9\n64\n79\n124\n95\n88\n135\n3\n51\n39\n6\n60\n108\n14\n35\n147\n89\n34\n65\n50\n145\n128"
+          )
+        ) should be(1885)
+      }
+    }
+
+    "numberPossibleArrangements" should {
+      "return the product of basic input" in {
+        AdapterArray.numberPossibleArrangements(
+          InputParser.parse[Int](
+            "1\n4\n5\n6\n7\n10\n11\n12\n15\n16\n19"
+          )
+        ) should be(8)
+      }
+
+      "return the product of basic input 2" in {
+        AdapterArray.numberPossibleArrangements(
+          InputParser.parse[Int](
+            "1\n2\n3\n4\n5\n8\n9\n10"
+          )
+        ) should be(26)
+      }
+
+      "return the product of example input" in {
+        AdapterArray.numberPossibleArrangements(
+          InputParser.parse[Int](
+            "28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3"
+          )
+        ) should be(19208)
+      }
+
+      "return the product of puzzle input" in {
+        AdapterArray.numberPossibleArrangements(
+          InputParser.parse[Int](
+            "80\n87\n10\n122\n57\n142\n134\n59\n113\n139\n101\n41\n138\n112\n46\n96\n43\n125\n36\n54\n133\n17\n42\n98\n7\n114\n78\n67\n77\n28\n149\n58\n20\n105\n31\n19\n18\n27\n40\n71\n117\n66\n21\n72\n146\n90\n97\n94\n123\n1\n119\n30\n84\n61\n91\n118\n2\n29\n104\n73\n13\n76\n24\n148\n68\n111\n131\n83\n49\n8\n132\n9\n64\n79\n124\n95\n88\n135\n3\n51\n39\n6\n60\n108\n14\n35\n147\n89\n34\n65\n50\n145\n128"
+          )
+        ) should be(2024782584832L)
+      }
+    }
+  }
+}
